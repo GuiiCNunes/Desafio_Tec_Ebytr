0c307353b496bbafc6c409db5d1234dfe8ba3789
Guilherme Nunes <guiicnunes@gmail.com>
2021-11-04 08:50:48 -0300

    FIX: Integration test now use mongo memory server

--- BACKEND/tests/integration.test.js
+++ BACKEND/tests/integration.test.js
@@ -1,14 +1,38 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
+const sinon = require('sinon');
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
 const server = require('../src/api/app');
 const payloads = require('./payloadTasks');
+const mongoConnection = require('../src/model/connection');
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Integration tests: ', () => {
+  before(async () => {
+    const DBServer = new MongoMemoryServer();
+    const URLMock = await DBServer.getUri();
+
+    connectionMock = MongoClient
+      .connect(URLMock, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+      .then((conn) => conn.db('Ebytr'));
+
+    
+    sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock);
+  });
+
+  after(() => {
+    mongoConnection.getConnection.restore();
+  });
+
   describe('when a task created', () => {
     let response = {};
 
