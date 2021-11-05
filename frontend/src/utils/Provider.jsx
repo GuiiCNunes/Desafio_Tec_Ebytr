655ad62fb46232e72c72790405375ebc2b20697c
Guilherme Nunes <guiicnunes@gmail.com>
2021-11-04 22:50:44 -0300

    DEV: Create a funcs in provider to manipule the context

--- frontend/src/utils/Provider.jsx
+++ frontend/src/utils/Provider.jsx
@@ -1,16 +1,47 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './AppContext';
+import Services from '../services';
 
 function Provider({ children }) {
   const [tasks, setTasks] = useState();
   const [taskToEdit, setTaskToEdit] = useState();
+
+  const addTask = async (task) => {
+    const response = await Services.addTask(task);
+    if (await response.status() === 201) {
+      const newTask = await response.json();
+      setTasks([
+        ...tasks,
+        newTask,
+      ]);
+    }
+  };
+  const deleteTask = async (id) => {
+    const response = await Services.deleteTask(id);
+    if (await response.status() === 200) {
+      setTasks(tasks.filter(({ _id }) => _id.toString() !== id));
+    }
+  };
+  const updateTask = async (task) => {
+    const { _id: id } = task;
+    const response = await Services.updateTask(task);
+    if (await response.status() === 200) {
+      const index = tasks.findIndex(({ _id }) => _id === id);
+      const newArray = [...tasks];
+      newArray[index] = task;
+      setTasks(newArray);
+    }
+  };
 
   const contextValue = {
     tasks,
     setTasks,
     taskToEdit,
     setTaskToEdit,
+    addTask,
+    deleteTask,
+    updateTask,
   };
 
   return (
